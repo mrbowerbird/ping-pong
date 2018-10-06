37280134d88a1e7eabcbece349a488b3de021f3a
Michael <mbutcherus@gmail.com>
2018-10-06 11:16:58 -0700

    Back end logic functions by way of console log.  Counts, returns ping, pong when appropriate.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
-
+var counter = function(userNumber) {
+  var output = [];
+  for (var countNumber = 1; countNumber <= userNumber; countNumber += 1) {
+    if (countNumber % 5 === 0 && countNumber % 3 === 0) {
+      output.push("pingpong");
+    } else if (countNumber % 5 === 0) {
+      output.push("pong");
+    } else if (countNumber % 3 === 0) {
+      output.push("ping");
+    } else {
+      output.push(countNumber);
+    }
+  }
+  console.log(output);
+};
 
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
     var userNumber = parseInt($("input#userNumber").val());
-
-    $("#result").text(userNumber);
+    var results = counter(userNumber)
+    console.log(userNumber)
+    $("#result").text(results);
   });
 });
